86741a85ad370d35a4f7fe79557541f995164251
gonzalocham <cham.gonzalo@gmail.com>
2019-11-21 20:46:26 -0300

    Initial Commit

---
+++ Anilist/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Anilist
+//
+//  Created by Gonzalo Cham on 21/11/2019.
+//  Copyright © 2019 Gonzalo Cham. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
